f41de21704ba0959bbd54171044ff38a0a664f96
Park Dongjin <jinidong@gmail.com>
2023-03-17 00:45:42 +0900

    HW2 Gaussian 1D complete

--- HW2/201824469_ParkDongJin_code.py
+++ HW2/201824469_ParkDongJin_code.py
@@ -12,5 +12,28 @@ def boxfilter(n):
 
 # print(boxfilter(3))
 # print(boxfilter(4))
-x = boxfilter(7)
-print(x)
+# print(boxfilter(7))
+
+def gauss1d(sigma):
+    width:int = int(math.ceil(6*sigma)) # 6*sigma의 다음 정수로 width를 설정
+    if width % 2 == 0: # 다음 정수가 짝수라면 +1 하여 홀수로 변경
+        width += 1
+    
+    center = math.floor(width / 2)  # 가운데 index를 획득
+    gaussian = np.zeros((1, width)).flatten()   # 반환할 1D Numpy Array 생성
+    for i in range(width):
+        gaussian[i] = i - center    # Hint:를 토대로 x값을 입력
+    for i in range(width):
+        gaussian[i] = math.exp(-gaussian[i]**2/(2*sigma**2)) # x값에 따라 gaussian값 획득 및 입력
+    
+    # 아래는 normalize, 전체 합으로 각 엔트리를 나눈다.
+    kernal_sum = np.sum(gaussian)
+    for i in range(width):
+        gaussian[i] /= kernal_sum
+    return gaussian
+
+# 결과 출력
+print("sigma = 0.3\n{}".format(gauss1d(0.3)))
+print("sigma = 0.5\n{}".format(gauss1d(0.5)))
+print("sigma = 1.0\n{}".format(gauss1d(1.0)))
+print("sigma = 2.0\n{}".format(gauss1d(2.0)))
