d1e6dad6f44fadeaaf62f70f25eac4845fdbd56a
Park Dongjin <jinidong@gmail.com>
2023-03-17 14:58:50 +0900

    Complete HW2 1-4-a

--- HW2/201824469_ParkDongJin_code.py
+++ HW2/201824469_ParkDongJin_code.py
@@ -46,6 +46,33 @@ def gauss2d(sigma):
     return np.outer(gauss1d(sigma), gauss1d(sigma))
 
 print("gauss2d(0.5) = \n{}".format(gauss2d(0.5)))
-print("gauss2d(1.0) = \n{}".format(gauss2d(1.0)))
+# print("gauss2d(1.0) = \n{}".format(gauss2d(1.0)))
 
+
+def convolove2d(array, filter):
+    padding_width = len(filter)-1 # filter에 따른 padding 크기
+    zero_padding = np.zeros((np.shape(array)[0] + padding_width, np.shape(array)[1] + padding_width))
+    # padding한 크기에 맞게 새로운 np.array를 생성
+
+    #convolution연산을 위해서 filter를 축의 방향에 대해서 뒤집는다.
+    flipped_filter = np.flip(filter, axis=0)
+    flipped_filter = np.flip(flipped_filter, axis=1)
+    # print(flipped_filter)
+
+    # 모든 entry에 대해서 paading을 제외한 부분에 array를 집어넣어 준다.
+    for i in range(1, np.shape(zero_padding)[0]-1):
+        for j in range(1, np.shape(zero_padding)[1]-1):
+            zero_padding[i][j] = array[i-1][j-1]
+
+    # 각 pixel을 순회하면서 각 entry에 필터를 연산한다.
+    for i in range(1, np.shape(zero_padding)[0]-1):
+        for j in range(1, np.shape(zero_padding)[1]-1):
+            subpart = zero_padding[i-1:i+2, j-1:j+2]
+            # print(subpart)
+            # print(flipped_filter)
+            zero_padding[i][j] = np.sum(subpart * flipped_filter)
+    # print(zero_padding)
+    # print(zero_padding[1:-1, 1:-1])
+
+    return zero_padding[1:-1, 1:-1]
 
