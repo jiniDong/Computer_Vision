8f47be54240000d9113c04245b1191a682d25258
Park Dongjin <jinidong@gmail.com>
2023-03-18 12:33:02 +0900

    Complete all tasks

--- HW2/201824469_ParkDongJin_code.py
+++ HW2/201824469_ParkDongJin_code.py
@@ -12,9 +12,10 @@ def boxfilter(n):
     return filter
 
 
-# print(boxfilter(3))
+print(boxfilter(3))
 # print(boxfilter(4))
-# print(boxfilter(7))
+print(boxfilter(7))
+
 
 def gauss1d(sigma):
     width: int = int(math.ceil(6 * sigma))  # 6*sigma의 다음 정수로 width를 설정
@@ -58,8 +59,6 @@ def convolove2d(array, filter):
     # convolution연산을 위해서 filter를 축의 방향에 대해서 뒤집는다.
     flipped_filter = np.flip(filter, axis=0)
     flipped_filter = np.flip(flipped_filter, axis=1)
-    # print(flipped_filter)
-
     # 모든 entry에 대해서 paading을 제외한 부분에 array를 집어넣어 준다.
     for i in range(padding_width, np.shape(zero_padding)[0] - padding_width):
         for j in range(padding_width, np.shape(zero_padding)[1] - padding_width):
@@ -69,17 +68,14 @@ def convolove2d(array, filter):
     for i in range(padding_width, np.shape(zero_padding)[0] - padding_width):
         for j in range(padding_width, np.shape(zero_padding)[1] - padding_width):
             subpart = zero_padding[i - padding_width:i + padding_width + 1, j - padding_width:j + padding_width + 1]
-            # print(subpart)
-            # print(flipped_filter)
             zero_padding[i][j] = np.sum(subpart * flipped_filter)
-    # print(zero_padding)
-    # print(zero_padding[1:-1, 1:-1])
 
     return zero_padding[padding_width:-padding_width, padding_width:-padding_width]
 
 
 def gaussconvolve2d(array, sigma):
     return convolove2d(array, gauss2d(sigma))
+
 
 # 파일 불러오기 및 가우시안 효과 적용
 dog_img = Image.open('hw2_image/2b_dog.bmp')
@@ -92,11 +88,63 @@ dog_img.show()
 dog_img_grey.show()
 Image.fromarray(blurred_dog_array).show()
 
+# Part 2: Hybrid Image
+# 1.
 
-# edge_dog_array = dog_img_array - blurred_dog_array
-# sharpened_dog_image = dog_img_array + edge_dog_array
-# Image.fromarray(sharpened_dog_image).show()
-#
-# blurred_dog = Image.fromarray(gaussconvolve2d(dog_img_array, 3.0))
-# blurred_dog.show()
+eiffel_img = Image.open('hw2_image/3a_eiffel.bmp')
+tower_img = Image.open('hw2_image/3b_tower.bmp')
 
+eiffel_img_array = np.asarray(eiffel_img)
+tower_img_array = np.asarray(tower_img)
+
+# print(eiffel_img_array)
+# print(tower_img_array)
+
+# Image.fromarray(eiffel_img_array).show()
+# Image.fromarray(tower_img_array).show()
+
+eiffel_height, eiffel_width, rgb = np.shape(eiffel_img_array)
+
+eiffel_img_array_r = eiffel_img_array[:, :,  :1].reshape(eiffel_height, eiffel_width)
+eiffel_img_array_g = eiffel_img_array[:, :, 1:2].reshape(eiffel_height, eiffel_width)
+eiffel_img_array_b = eiffel_img_array[:, :, 2:3].reshape(eiffel_height, eiffel_width)
+
+eiffel_img_array_r_blurred = gaussconvolve2d(eiffel_img_array_r, 3.0)
+eiffel_img_array_g_blurred = gaussconvolve2d(eiffel_img_array_g, 3.0)
+eiffel_img_array_b_blurred = gaussconvolve2d(eiffel_img_array_b, 3.0)
+
+eiffel_img_blurred = np.zeros((eiffel_height, eiffel_width, rgb))
+for row in range(eiffel_height):
+    for pixel in range(eiffel_width):
+        eiffel_img_blurred[row][pixel] = np.array([eiffel_img_array_r_blurred[row][pixel],
+                                                   eiffel_img_array_g_blurred[row][pixel],
+                                                   eiffel_img_array_b_blurred[row][pixel]])
+
+Image.fromarray(eiffel_img_blurred.astype(np.uint8)).show()
+
+# 2.
+
+tower_height, tower_width, rgb = np.shape(tower_img_array)
+
+tower_img_array_r = tower_img_array[:, :,  :1].reshape(tower_height, tower_width)
+tower_img_array_g = tower_img_array[:, :, 1:2].reshape(tower_height, tower_width)
+tower_img_array_b = tower_img_array[:, :, 2:3].reshape(tower_height, tower_width)
+
+tower_img_array_r_blurred = gaussconvolve2d(tower_img_array_r, 3.0)
+tower_img_array_g_blurred = gaussconvolve2d(tower_img_array_g, 3.0)
+tower_img_array_b_blurred = gaussconvolve2d(tower_img_array_b, 3.0)
+
+tower_img_sharpened = np.zeros((eiffel_height, eiffel_width, rgb))
+for row in range(tower_height):
+    for pixel in range(tower_width):
+        tower_img_sharpened[row][pixel] = np.array([tower_img_array_r[row][pixel] - tower_img_array_r_blurred[row][pixel],
+                                                    tower_img_array_g[row][pixel] - tower_img_array_g_blurred[row][pixel],
+                                                    tower_img_array_b[row][pixel] - tower_img_array_b_blurred[row][pixel]])
+
+tower_img_show = tower_img_sharpened + 128
+Image.fromarray(tower_img_show.astype(np.uint8)).show()
+
+# 3
+
+hybrid = (tower_img_sharpened + eiffel_img_blurred + 128)/2
+Image.fromarray(hybrid.astype(np.uint8)).show()
